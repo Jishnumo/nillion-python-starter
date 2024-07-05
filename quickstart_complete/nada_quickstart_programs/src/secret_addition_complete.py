ab1a71d0266548247692904d71c247904bbd770f
Jishnumo <jishnumohan292@gmail.com>
2024-07-05 10:15:06 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,22 @@
 from nada_dsl import *
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("telemetry")
+
 def nada_main():
-
+    telemetry_id = "telemetry_id_123"
+    logger.info(f"Telemetry enabled with ID: {telemetry_id}")
+    
     party1 = Party(name="Party1")
-
+    
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    
+    sum_int = my_int1 + my_int2
+    
+    return [
+        Output(sum_int, "sum_output", party1)
+    ]
 
-    new_int = my_int1 + my_int2
-
-    return [Output(new_int, "my_output", party1)]
+nada_main()
